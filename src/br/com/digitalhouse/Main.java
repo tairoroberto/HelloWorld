c0b5d50ee69cf38f28e9cfb1f3f71cbe327a91a8
unknown <Digital@MASTER-1.dhbrasil.digitalhouse.com>
2019-04-03 22:35:46 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Main.java
@@ -0,0 +1,17 @@
+package br.com.digitalhouse;
+
+public class Main {
+    public static void main(String[] args){
+
+        int[] numeros = new int[4];
+        numeros[0] = 1;
+        numeros[1] = 23;
+        numeros[2] = 2;
+        numeros[3] = 12;
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+    }
+}
